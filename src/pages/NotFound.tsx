2e7334657fda68811d1e6a419e24b2393e05ff62
Andreas <and@reas.me>
2024-04-28 17:32:19 -0700

    Refine 404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,3 +1,5 @@
+import * as routes from '../routes.ts';
+import { Link } from "react-router-dom";
 import { Box, Typography } from "@mui/material";
 
 function NotFound() {
@@ -10,9 +12,15 @@ function NotFound() {
             justifyContent="center"
             justifyItems="center"
             width="100%"
+            flexDirection="column"
         >
             <Typography variant="h2">
                 404 Not Found!
+            </Typography>
+            <Typography variant="h1" component="div" noWrap>
+                <Link to={routes.home} style={{ textDecoration: 'none', color: 'inherit' }}>
+                    🏠
+                </Link>
             </Typography>
         </Box>
     );
